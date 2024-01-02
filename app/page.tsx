46f8eacdfbe5bb88f45bfc9952d266a81feb65cd
lapinRagnar <lapinragnar@gmail.com>
2024-01-02 19:49:57 +0100

    page d'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@ import {Button} from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div className="text-green-500">
-      salut
-      <Button
-        variant={"maVariante"}
-      >
-        cliquer moi
-      </Button>
-    </div>
+    <main 
+      className="
+        flex h-full flex-col items-center justify-center
+        bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))]
+        from-neutral-700 to-orange-800
+      ">
+        <div className='space-y-6 text-center'>
+          <h1 className='text-6xl font-semibold text-white drop-shadow-md'>🔏 Auth</h1>
+          <p className='text-gray-300 text-lg'>A simple authentication service</p>          
+        </div>
+    </main>
   )
 }
