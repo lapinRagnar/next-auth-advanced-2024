d10b56e66e9ea0df0bc1d36812bdfd6c44aa07c8
lapinRagnar <lapinragnar@gmail.com>
2024-01-02 20:31:47 +0100

    login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Poppins } from 'next/font/google'
 import {Button} from '@/components/ui/button'
 import { cn } from '@/lib/utils'
+import LoginButton from '@/components/auth/LoginButton'
+
 
 const font = Poppins({ weight: ['600'], subsets: ['latin'] })
 
@@ -21,7 +23,10 @@ export default function Home() {
             🔏 Auth
           </h1>
           <p className='text-gray-300 text-lg'>A simple authentication service</p>
-          <Button variant={'secondary'} size={'lg'}>Sign In</Button>          
+
+          <LoginButton>
+            <Button variant={'secondary'} size={'lg'}>Sign In</Button>          
+          </LoginButton>
         </div>
     </main>
   )
