2939d6d9f20f529c5f7d628b04136b99c0ad6cde
lapinRagnar <lapinragnar@gmail.com>
2024-01-03 09:12:21 +0100

    suite

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,5 +1,9 @@
+'use client'
+
+import * as z from 'zod'
 import {useForm} from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { LoginSchema } from '@/schemas'
 import {
   Form,
   FormControl,
@@ -12,6 +16,15 @@ import {
 import { CardWrapper } from "@/components/auth/CardWrapper"
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
+
+
   return (
     <CardWrapper
       headerLabel="Welcome Back"
