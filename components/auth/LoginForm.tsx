736bd739c72f3f8db5e825a556278df63eaf590a
lapinRagnar <lapinragnar@gmail.com>
2024-01-03 09:06:32 +0100

    suite form

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,3 +1,14 @@
+import {useForm} from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+
 import { CardWrapper } from "@/components/auth/CardWrapper"
 
 export const LoginForm = () => {
