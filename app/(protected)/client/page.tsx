afa84647b80c3d7186c51eaab656322c4bd6de67
lapinRagnar <lapinragnar@gmail.com>
2024-01-05 15:18:27 +0100

    Client UserInfo

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { UserInfo } from "@/components/UserInfo"
+import { useCurrentUser } from "@/hooks/useCurrentUser"
+
+const ClientPage = () => {
+  const user = useCurrentUser()
+
+  return ( 
+    <UserInfo
+      label="📱 Client component"
+      user={user}
+    />
+   );
+}
+ 
+export default ClientPage
